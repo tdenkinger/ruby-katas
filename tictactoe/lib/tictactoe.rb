67475767990106d56e0a693ac80909ba36919543
Troy Denkinger <tdenkinger@gmail.com>
2012-08-23 19:19:22 -0400

    Refactored to have more general interfaces.

--- tictactoe/lib/tictactoe.rb
+++ tictactoe/lib/tictactoe.rb
@@ -2,42 +2,53 @@ class TicTacToe
   attr_reader :board, :players
 
   def initialize
-    @board = [0, 1, 2, 3, 4, 5, 6, 7, 8]   
+    @board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     @players = { :player_1 => [], :player_2 => []}
 
     @winning_combos = [ "012", "345", "678", "036", "147", "258", "048", "246" ]
   end
 
-  def take_spot(player, spot_to_take)
-    return "Spot is not on the board" if spot_to_take > @board.length 
-    return "Spot is already taken, choose again" if @board[spot_to_take].nil?
-    @players[player] << spot_to_take 
+  def take_square(player, spot_to_take)
+    return "Square is not on the board" if spot_to_take > @board.length
+    return "Square is already taken, choose again" if @board[spot_to_take].nil?
+    @players[player] << spot_to_take
     @players[player].sort!
     @board[spot_to_take] = nil
     return "#{player} wins!" if is_winner?(player)
-    return "The game is hung" if is_hung? 
+    return "The game is hung" if is_hung?
+    "no winner"
   end
 
-  def number_of_unclaimed_spots 
+  def number_of_unclaimed_spots
     board.compact.count
   end
-
-  private
 
   def is_winner?(player)
     status = false
     players[player].combination(3).to_a.each do | combo |
       status = is_winning_combination?(combo.join)
-      break if status 
+      break if status
     end
-    status 
+    status
   end
-  
+
   def is_hung?
     @board.compact.empty? ? true : false
   end
- 
+
   def is_winning_combination?(combo)
     @winning_combos.include?(combo) ? true : false
+  end
+
+  def is_player_clear?(player)
+    @players[player].count == 0 ? true : false
+  end
+
+  def is_board_clear?
+    number_of_unclaimed_spots == @board.count ? true : false
+  end
+
+  def has_square?(player, square)
+    @players[player].index(square) ? true : false
   end
 end
--- tictactoe/spec/tictactoe_spec.rb
+++ tictactoe/spec/tictactoe_spec.rb
@@ -1,130 +1,132 @@
 require_relative "../lib/tictactoe.rb"
 
 describe TicTacToe do
-  let(:t){ TicTacToe.new }
-  let(:total_squares){9}
+  let(:game){ TicTacToe.new }
 
   context "before any moves have been made" do
     context "the board" do
-      it "has a full bag of spots" do
-        t.board.count.should == total_squares
+      it "has no taken squares" do
+        game.is_board_clear?.should be true
       end
     end
-  
-    context "player 1" do
-      it "has an empty bag" do
-        t.players[:player_1].empty?.should be true 
-      end
-    end 
 
-    context "player 2" do
-      it "has an empty bag" do
-        t.players[:player_2].empty?.should be true
+    context "players" do
+      it "Player 1 has taken no squares" do
+        game.is_player_clear?(:player_1).should be true
       end
-    end 
+
+      it "Player 2 has taken no squares" do
+        game.is_player_clear?(:player_2).should be true
+      end
+    end
   end
 
   context "after one pair of moves" do
     context "the board" do
       it "has 7 spots remaining" do
         expect {
-          t.take_spot(:player_1, 0)
-          t.take_spot(:player_2, 1)
-        }.to change{t.number_of_unclaimed_spots}.by(-2)
+          game.take_square(:player_1, 0)
+          game.take_square(:player_2, 1)
+        }.to change{game.number_of_unclaimed_spots}.by(-2)
       end
     end
-    
-    context "player 1 trieds to take an unavailable spot" do
-      context "when the spot has already been taken" do
-        it "does not add the spot to the player" do
+
+    context "a player tries to take an unavailable square" do
+      context "when the square has already been taken" do
+        it "does not allow the player to have the square" do
           expect {
-            t.take_spot(:player_1, 5)
-            t.take_spot(:player_2, 5)
-          }.to change{t.number_of_unclaimed_spots}.by(-1)
+            game.take_square(:player_1, 5)
+            game.take_square(:player_2, 5)
+          }.to change{game.number_of_unclaimed_spots}.by(-1)
         end
       end
 
-      context "when the spot is not on the board" do
-        it "does not add the spot to the player" do
-          t.take_spot(:player_1, 15)
-          t.players[:player_1].count.should == 0
-          t.board.compact.count.should == 9
+      context "when the square is not on the board" do
+        it "does not add the square to the player" do
+          game.take_square(:player_1, 15).should == "Square is not on the board"
+          game.has_square?(:player_1, 15).should be false
+        end
+
+        it "does not remove a square from availability" do
+          expect{
+            game.take_square(:player_1, 15)
+          }.to_not change{game.number_of_unclaimed_spots}
         end
       end
-    end 
+    end
 
     context "each player" do
-      it "has one spot" do
-        t.take_spot(:player_1, 0)
-        t.take_spot(:player_2, 1)
-        t.players[:player_1].count.should == 1
-        t.players[:player_2].count.should == 1
+      it "has one square" do
+        game.take_square(:player_1, 0)
+        game.take_square(:player_2, 1)
+        game.has_square?(:player_1, 0).should == true
+        game.has_square?(:player_2, 1).should == true
       end
-    end 
+    end
   end
- 
+
   context "after one player has three spots" do
     context "the player has a non-winning set of spots" do
       it "the player does not win" do
-        t.take_spot(:player_1, 3).should == nil
-        t.take_spot(:player_1, 0).should == nil
-        t.take_spot(:player_1, 1).should == nil
+        game.take_square(:player_1, 3).should == "no winner"
+        game.take_square(:player_1, 0).should == "no winner"
+        game.take_square(:player_1, 1).should == "no winner"
       end
     end
 
     context "The player has a winning set of spots" do
       it "the player wins" do
-        t.take_spot(:player_1, 0).should == nil
-        t.take_spot(:player_1, 2).should == nil
-        t.take_spot(:player_1, 1).should == "player_1 wins!" 
+        game.take_square(:player_1, 0).should == "no winner"
+        game.take_square(:player_1, 2).should == "no winner"
+        game.take_square(:player_1, 1).should == "player_1 wins!"
       end
     end
-  end 
+  end
 
   context "after one player has four spots" do
     context "the player has a non-winning set of spots" do
       it "the player does not win" do
-        t.take_spot(:player_1, 3).should == nil
-        t.take_spot(:player_1, 0).should == nil
-        t.take_spot(:player_1, 1).should == nil
-        t.take_spot(:player_1, 5).should == nil
+        game.take_square(:player_1, 3).should == "no winner"
+        game.take_square(:player_1, 0).should == "no winner"
+        game.take_square(:player_1, 1).should == "no winner"
+        game.take_square(:player_1, 5).should == "no winner"
       end
     end
 
     context "The player has a winning set of spots" do
       it "the player wins" do
-        t.take_spot(:player_2, 0).should == nil
-        t.take_spot(:player_2, 2).should == nil
-        t.take_spot(:player_2, 5).should == nil
-        t.take_spot(:player_2, 1).should == "player_2 wins!"
+        game.take_square(:player_2, 0).should == "no winner"
+        game.take_square(:player_2, 2).should == "no winner"
+        game.take_square(:player_2, 5).should == "no winner"
+        game.take_square(:player_2, 1).should == "player_2 wins!"
       end
     end
   end
-  
+
   context "When alternating plays" do
     context "player 1" do
       it "wins" do
-        t.take_spot(:player_1, 0).should == nil
-        t.take_spot(:player_2, 1).should == nil
-        t.take_spot(:player_1, 4).should == nil
-        t.take_spot(:player_2, 2).should == nil
-        t.take_spot(:player_1, 6).should == nil
-        t.take_spot(:player_2, 7).should == nil
-        t.take_spot(:player_1, 8).should == "player_1 wins!" 
+        game.take_square(:player_1, 0).should == "no winner"
+        game.take_square(:player_2, 1).should == "no winner"
+        game.take_square(:player_1, 4).should == "no winner"
+        game.take_square(:player_2, 2).should == "no winner"
+        game.take_square(:player_1, 6).should == "no winner"
+        game.take_square(:player_2, 7).should == "no winner"
+        game.take_square(:player_1, 8).should == "player_1 wins!"
       end
     end
 
     context "neither player can win" do
       it "the game is hung" do
-        t.take_spot(:player_1, 0).should == nil
-        t.take_spot(:player_2, 4).should == nil
-        t.take_spot(:player_1, 1).should == nil
-        t.take_spot(:player_2, 2).should == nil
-        t.take_spot(:player_1, 6).should == nil
-        t.take_spot(:player_2, 7).should == nil
-        t.take_spot(:player_1, 5).should == nil 
-        t.take_spot(:player_2, 3).should == nil 
-        t.take_spot(:player_1, 8).should == "The game is hung" 
+        game.take_square(:player_1, 0).should == "no winner"
+        game.take_square(:player_2, 4).should == "no winner"
+        game.take_square(:player_1, 1).should == "no winner"
+        game.take_square(:player_2, 2).should == "no winner"
+        game.take_square(:player_1, 6).should == "no winner"
+        game.take_square(:player_2, 7).should == "no winner"
+        game.take_square(:player_1, 5).should == "no winner"
+        game.take_square(:player_2, 3).should == "no winner"
+        game.take_square(:player_1, 8).should == "The game is hung"
       end
     end
   end
