5dc4ba7e4dc67ab816668d898fdb13e0739f3104
Troy Denkinger <tdenkinger@gmail.com>
2012-09-23 12:11:18 -0400

    Refactored to make some of the logic more clear. Better encapsulation as well.

--- tictactoe/features/new_game.feature
+++ tictactoe/features/new_game.feature
@@ -3,7 +3,7 @@ Feature: Setup a new game
   Scenario: When a new game is started
     Given This is a fresh game
     Then All of the spots are empty
-  
+
   Scenario: When a new game is started
     Given This is a fresh game
     Then Player 1 should have no chosen squares
--- tictactoe/lib/tictactoe.rb
+++ tictactoe/lib/tictactoe.rb
@@ -1,31 +1,57 @@
 class TicTacToe
-  attr_reader :board, :players
-
   def initialize
-    @board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    @players = { :player_1 => [], :player_2 => []}
-
+    @board = (0..8).to_a
+    @total_number_of_squares = @board.length
     @winning_combos = [ "012", "345", "678", "036", "147", "258", "048", "246" ]
+
+    @players = { :player_1 => [], :player_2 => []}
   end
 
   def take_square(player, square_to_take)
-    return "Square is not on the board" if square_to_take > @board.length
-    return "Square is already taken, choose again" if @board[square_to_take].nil?
-    @players[player] << square_to_take
+    square_is_available, message = is_square_available?(square_to_take)
+    if square_is_available
+      give_player_square(player, square_to_take)
+      update_board(square_to_take)
+      message = test_for_winner(player)
+    end
+    message
+  end
+
+  def update_board(square)
+    @board.delete(square)
+  end
+
+  def give_player_square(player, square)
+    @players[player] << square
     @players[player].sort!
-    @board[square_to_take] = nil
-    return "#{player} wins!" if is_winner?(player)
-    return "The game is hung" if is_hung?
-    "no winner"
+  end
+
+  def test_for_winner(player)
+    messages = { winner:    "#{player} wins!",
+                 hung:      "The game is hung",
+                 no_winner: "no winner"}
+
+    return messages[:winner] if is_winner?(player)
+    return messages[:hung]   if is_hung?
+    messages[:no_winner]
+  end
+
+  def is_square_available?(square_to_take)
+    messages = {not_on_board: "Square is not on the board",
+                taken:        "Square is already taken, choose again"}
+
+    return false, messages[:not_on_board] if square_to_take > @total_number_of_squares
+    return false, messages[:taken]        unless @board.include?(square_to_take)
+    true
   end
 
   def number_of_unclaimed_spots
-    board.compact.count
+    @board.count
   end
 
   def is_winner?(player)
     status = false
-    players[player].combination(3).to_a.each do | combo |
+    @players[player].combination(3).to_a.each do | combo |
       status = is_winning_combination?(combo.join)
       break if status
     end
@@ -33,7 +59,7 @@ class TicTacToe
   end
 
   def is_hung?
-    @board.compact.empty? ? true : false
+    @board.empty? ? true : false
   end
 
   def is_winning_combination?(combo)
